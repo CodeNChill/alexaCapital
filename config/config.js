2090ebfcd1def83c653bdc41121c90b79f46e2a9
Danny V <Danny V>
2017-03-13 20:06:39 -0500

    Added session secret to theh config file

--- config/config.js
+++ config/config.js
@@ -35,9 +35,9 @@ var config = {
     port: 3000,
     CLIENT_ID:'8955da598bcd4e42ae4de3bf79e0605d',
     CLIENT_SECRET:'62bcb17543d005b1b84b1ee9f0e44e76',
-    REDIRECT_URI:'http://localhost:3000/authredirect',  //Replace this with the redirect uri entered in the developer portal
+    REDIRECT_URI:'http://localhost:3000/authredirect',  
     BASE_URI:'https://api-sandbox.capitalone.com',
-    SESSION_SECRET: 'This should be changed and not published anywhere!' //Change this to a random string
+    SESSION_SECRET: 'Code and Chill with Mike and the B Team!' //Change this to a random string
   },
 
   test: {
