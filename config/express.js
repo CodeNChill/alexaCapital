2d8d790579bb92bdea9ec9ee8cfae263d4a322dc
David Diaz <indiandavy@gmail.com>
2017-03-13 19:59:47 -0500

    Modified Config

--- config/express.js
+++ config/express.js
@@ -1,3 +1,21 @@
+ /*
+File name: express.js
+Description: Express setup for external rewards reference application.
+
+Copyright 2016 Capital One Services, LLC
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
 var express = require('express');
 var glob = require('glob');
 
@@ -7,6 +25,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compress = require('compression');
 var methodOverride = require('method-override');
+var session = require('express-session')
+var uuid = require('uuid');
+var helmet = require('helmet');
 
 module.exports = function(app, config) {
   var env = process.env.NODE_ENV || 'development';
@@ -17,6 +38,7 @@ module.exports = function(app, config) {
   app.set('view engine', 'ejs');
 
   // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(helmet());
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
@@ -26,6 +48,15 @@ module.exports = function(app, config) {
   app.use(compress());
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
+  app.use(session({
+    genid: function(req) {
+      return uuid.v4(); // use UUIDs for session IDs 
+    },
+    secret: config.SESSION_SECRET,
+    name: 'sessionId',
+    resave: false,
+    saveUninitialized: false
+  }));
 
   var controllers = glob.sync(config.root + '/app/controllers/*.js');
   controllers.forEach(function (controller) {
@@ -58,5 +89,4 @@ module.exports = function(app, config) {
       });
   });
 
-  return app;
 };
